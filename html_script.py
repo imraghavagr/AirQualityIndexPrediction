c1dd66349c7052d309b779ea827c5fcdbcc4b2fe
Raghav Agarwal <raghavagrawal406@gmail.com>
2021-08-27 18:51:44 +0530

    some changes

--- html_script.py
+++ html_script.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 21:34:41 2019
+
+@author: krish.naik
+"""
 import os
 import time
 import requests
@@ -5,28 +11,28 @@ import sys
 
 
 def retrieve_html():
-    for year in range(2013, 2021):
-        for month in range(1, 13):
-            if month < 10:
-                url = "https://en.tutiempo.net/climate/0{}-{}/ws-432950.html".format(month, year)
+    for year in range(2013,2019):
+        for month in range(1,13):
+            if(month<10):
+                url='http://en.tutiempo.net/climate/0{}-{}/ws-421820.html'.format(month
+                                                                          ,year)
             else:
-                url = "https://en.tutiempo.net/climate/{}-{}/ws-432950.html".format(month, year)
-
-            texts = requests.get(url)
-            # utf encoding
-            text_utf = texts.text.encode('utf=8')
-
-            if not os.path.exists("./data/html_Data/{}".format(year)):
-                os.makedirs("./data/html_Data/{}".format(year))
-
-            with open("./data/html_Data/{}/{}.html".format(year, month), "wb") as output:
+                url='http://en.tutiempo.net/climate/{}-{}/ws-421820.html'.format(month
+                                                                          ,year)
+            texts=requests.get(url)
+            text_utf=texts.text.encode('utf=8')
+            
+            if not os.path.exists("Data/Html_Data/{}".format(year)):
+                os.makedirs("Data/Html_Data/{}".format(year))
+            with open("Data/Html_Data/{}/{}.html".format(year,month),"wb") as output:
                 output.write(text_utf)
-
-            sys.stdout.flush()
-
-
-if __name__ == "__main__":
-    start_time = time.time()
+            
+        sys.stdout.flush()
+        
+if __name__=="__main__":
+    start_time=time.time()
     retrieve_html()
-    stop_time = time.time()
-    print("Time taken {}".format(stop_time - start_time))
+    stop_time=time.time()
+    print("Time taken {}".format(stop_time-start_time))
+        
+    
